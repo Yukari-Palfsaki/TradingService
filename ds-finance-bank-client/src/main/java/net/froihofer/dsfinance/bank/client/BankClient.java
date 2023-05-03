c4078fdfe1e31c7ab0e984a39102ad6e38e0b619
yukarisusaki <yukarisoprano717@gmail.com>
2023-05-03 16:01:52 +0200

    WSDL - history is not working yet.

--- ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/BankClient.java
+++ ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/BankClient.java
@@ -1,13 +1,25 @@
 package net.froihofer.dsfinance.bank.client;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Scanner;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import net.froihofer.ejb.bank.common.Bank;
+//import net.froihofer.ejb.bank.common.BankException;
+import net.froihofer.ejb.bank.common.JaxRsAuthenticator;
+import net.froihofer.ejb.bank.common.PublicStockQuoteDTO;
 import net.froihofer.util.AuthCallbackHandler;
 import net.froihofer.util.WildflyJndiLookupHelper;
 import org.slf4j.Logger;
@@ -19,6 +31,14 @@ import org.slf4j.LoggerFactory;
  */
 public class BankClient {
   private static Logger log = LoggerFactory.getLogger(BankClient.class);
+  private Client client;
+  private WebTarget baseTarget;
+  public void setupRest (){
+    client = ClientBuilder.newClient()
+        .register(new JaxRsAuthenticator("customer1", "customer1Pass"))
+        .register(JacksonJsonProvider.class);
+    baseTarget = client.target ("http://localhost:8080/ds-finance-bank-web/rs/bank");
+  }
 
   /**
    * Skeleton method for performing an RMI lookup
@@ -44,38 +64,100 @@ public class BankClient {
 
   private void run() {
     Bank bank = getRmiProxy();
+    setupRest();
     System.out.println("Client Test");
     Scanner scanner = new Scanner(System.in);
 
     while(true) {
-      System.out.println("Search or buy?");
-      var option = scanner.nextLine().toLowerCase();
-      if(option.equals("search")) {
-        System.out.println("Enter name to search: ");
-        var name = scanner.nextLine();
-        var test = bank.findStockByName(name);
-        System.out.println("Search result");
-        for(int i = 0; i < test.size(); i++) {
-          System.out.printf("%s: %s\n", test.get(i).getCompanyName(), test.get(i).getSymbol());
-        }
-      } else if(option.equals("buy")) {
-        System.out.println("Enter symbol: ");
-        var symbol = scanner.nextLine();
-        System.out.println("Enter amount: ");
-        var amount = scanner.nextLine();
-        try {
-          int shares = Integer.parseInt(amount);
-          var result = bank.buyStockByName(symbol, shares);
-          System.out.println("Cost per share: " + result);
-          System.out.println("Overall cost: " + result.multiply(BigDecimal.valueOf(shares)));
+      System.out.println("Choose option: Search, sell, buy or history.");
 
-        } catch (Exception e) {
-          e.printStackTrace();
+      var option = scanner.nextLine().toLowerCase();
+      // Options for search, sell, buy
+      switch (option) {
+        case "search": {
+          System.out.println("Enter name to search: ");
+          var name = scanner.nextLine();
+          var test = bank.findStockByName(name);
+          System.out.println("Search result");
+          for(int i = 0; i < test.size(); i++) {
+            System.out.printf("%s: %s\n", test.get(i).getCompanyName(), test.get(i).getSymbol());
+          }
+          break;
+        }
+
+        case "sell": {
+          System.out.println("Enter symbol: ");
+          var symbol = scanner.nextLine();
+          System.out.println("Enter amount: ");
+          var amount = scanner.nextLine();
+          try {
+
+            int shares = Integer.parseInt(amount);
+
+            WebTarget getTarget = baseTarget.path("sell")
+                .queryParam("symbol", symbol)
+                .queryParam("amount", shares);
+
+            var response = getTarget.request(MediaType.APPLICATION_JSON_TYPE)
+                .get();
+
+            if(response.getStatus() != Response.Status.OK.getStatusCode()) {
+              throw new WebApplicationException(response.getStatusInfo().getReasonPhrase());
+            }
+
+            var res = response.readEntity(BigDecimal.class);
+
+            System.out.println("Cost per share: " + res);
+            System.out.println("Overall cost: " + res.multiply(BigDecimal.valueOf(shares)));
+          }  catch (Exception e) {
+            log.error("Something did not work, see stack trace.", e);
+            e.printStackTrace();
+          }
+          break;
+        }
+        case "buy": {
+          System.out.println("Enter symbol: ");
+          var symbol = scanner.nextLine();
+          System.out.println("Enter amount: ");
+          var amount = scanner.nextLine();
+          try {
+            int shares = Integer.parseInt(amount);
+            var result = bank.buyStockByName(symbol, shares);
+            System.out.println("Cost per share: " + result);
+            System.out.println("Overall cost" + result.multiply(BigDecimal.valueOf(shares)));
+          } catch (Exception e) {
+            log.error("Something did not work, see stack trace." + e);
+            e.printStackTrace();
+          }
+          break;
+        }
+        case "hisotry": {
+          System.out.println("Enter symbol: ");
+          var symbol = scanner.nextLine();
+          try {
+            WebTarget getTarget = baseTarget.path("history")
+                .queryParam("symbol", symbol);
+
+            var response = getTarget.request (MediaType.APPLICATION_JSON_TYPE).get();
+            if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+              throw new WebApplicationException(response.getStatusInfo().getReasonPhrase());
+            }
+            var res = response.readEntity(new GenericType<ArrayList<PublicStockQuoteDTO>>(){});
+            System.out.println("History result");
+            for (int i = 0; i < res.size(); i++){
+              System.out.printf("companyName: %s: Symbol: %s Available shares: %d lastTradePrice: %s lastTradeTime: %s\n",
+                  res.get(i).getCompanyName(), res.get(i).getSymbol(), res.get(i).getFloatShares(),
+                  res.get(i).getLastTradePrice().toString(), res.get(i).getLastTradeTimeAsDate());
+            }
+
+          }catch (Exception e){
+            log.error( ("Something did not work, see stack trace."));
+            e.printStackTrace();
+          }
+          break;
         }
       }
     }
-
-
     //TODO implement the client part
   }
 
--- ds-finance-bank-common/src/main/java/net/froihofer/ejb/bank/common/BankException.java
+++ ds-finance-bank-common/src/main/java/net/froihofer/ejb/bank/common/BankException.java
@@ -1,4 +1,17 @@
 package net.froihofer.ejb.bank.common;
 
-public class BankException {
+public class BankException extends java.lang.Exception{
+  public BankException(){
+
+  }
+  public BankException(String msg){
+    super (msg);
+  }
+
+  public BankException (String msg, Throwable cause){
+    super (msg, cause);
+  }
+  public BankException (Throwable cause){
+    super (cause);
+  }
 }
